e48ed42829616a11464e0fa2979cd25c0d5e4b7e
localadmin <localadmin@MONILJ-MAC.local>
2021-02-27 10:36:04 +0530

    Added a method for desired capabilities | Wrote a single flow for youtube add

--- Automation_for_Native_application/Basic_LaunchApp_test.py
+++ Automation_for_Native_application/Basic_LaunchApp_test.py
@@ -1,14 +1,24 @@
 from pathlib import Path
 from appium import webdriver
 import unittest
-desired_caps = dict(
-    platformName='Android',
-    platformVersion='9',
-    automationName='uiautomator2',
-    deviceName='Pixel 2 API 28 2',
-    appPackage='com.google.android.youtube',
-    appActivity='com.google.android.apps.youtube.app.WatchWhileActivity'
-)
-driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-el = driver.find_element_by_accessibility_id('Search')
+
+
+def des_cap():
+    desired_caps = dict(
+        platformName='Android',
+        platformVersion='9',
+        automationName='uiautomator2',
+        deviceName='Pixel 2 API 28 2',
+        appPackage='com.google.android.youtube',
+        appActivity='com.google.android.apps.youtube.app.WatchWhileActivity')
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    return driver
+
+
+drive_instance = des_cap()
+el = drive_instance.find_element_by_accessibility_id('Search')
 el.click()
+edit_box = drive_instance.find_element_by_id('search_edit_text')
+edit_box.click()
+edit_box.send_keys("Appium automation")
+drive_instance.find_element_by_id('com.google.android.youtube:id/text').click()

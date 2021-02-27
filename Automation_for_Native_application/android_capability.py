c6bb627be96dc2089981dd23cf066656f603995d
localadmin <localadmin@MONILJ-MAC.local>
2021-02-27 12:55:42 +0530

    Added capabilities for Learn Android with Source Code Application

--- Automation_for_Native_application/android_capability.py
+++ Automation_for_Native_application/android_capability.py
@@ -3,7 +3,7 @@ from appium import webdriver
 import unittest
 
 
-def des_cap():
+def des_cap_for_youtube():
     desired_caps = dict(
         platformName='Android',
         platformVersion='9',
@@ -11,5 +11,17 @@ def des_cap():
         deviceName='Pixel 2 API 28 2',
         appPackage='com.google.android.youtube',
         appActivity='com.google.android.apps.youtube.app.WatchWhileActivity')
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    return driver
+
+
+def des_cap_for_learn_android_with_source_code():
+    desired_caps = dict(
+        platformName='Android',
+        platformVersion='9',
+        automationName='uiautomator2',
+        deviceName='Pixel 2 API 28 2',
+        appPackage='com.tutorials.learn.androidexample',
+        appActivity='com.tutorials.learn.androidexample.MainActivity')
     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
     return driver
